107cda117f94115a1d49a8e348d7f1cdef1067e1
rick Deakins <rdeakins96@gmail.com>
2022-10-11 20:40:34 -0400

    local storage created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,24 @@
 var rightNow = moment().format("dddd, MMMM Do");
 $('#currentDay').append(rightNow);
+
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Saves text input into local storage
+        localStorage.setItem(time, text);
+    })
+
+    // Loads text input from local storage
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+});
