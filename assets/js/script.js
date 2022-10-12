b18aa468d4c44a3660d0aa91b31296993d72fdb1
rick Deakins <rdeakins96@gmail.com>
2022-10-11 21:46:19 -0400

    time tracking created

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,4 +21,34 @@ $(document).ready(function () {
     $("#hour15 .description").val(localStorage.getItem("hour15"));
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+    function timeTracker() {
+        // gets the current time
+        var timeNow = moment().hour();
+
+        // loop time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            // check the time and add the classes for background indicators
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+
+    timeTracker();
 });
